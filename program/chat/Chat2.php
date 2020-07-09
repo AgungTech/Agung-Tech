d16d73308dc60d1bb57768a351f093555fc9750d
Miftachul <huda_makruf@outlook.com>
2020-07-09 16:42:25 +0700

    Add files via upload

---
+++ program/chat/Chat2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML PUBLIC>
+<HTML>
+<HEAD>
+<TITLE>Agung Computer Day Chat</TITLE>
+</HEAD>
+<FRAMESET cols="20%, 80%">
+  <FRAMESET rows="100, 200">
+      <FRAME src="icon.png">
+      <FRAME src="menu.swf">
+  </FRAMESET>
+  <FRAME src="http://www.everywherechat.com/everywherechat.swf?defaultRoom=Agung_Computer&roomList=false&fontSize=12&width=900&height=700&theme=day">
+  <NOFRAMES>
+      <P>This frameset document contains:
+      <UL>
+         <LI><A href="">Some neat contents</A>
+         <LI><IMG src="contents_of_frame2.gif" alt="A neat image">
+         <LI><A href="contents_of_frame3.html">Some other neat contents</A>
+      </UL>
+  </NOFRAMES>
+</FRAMESET>
+</HTML>
